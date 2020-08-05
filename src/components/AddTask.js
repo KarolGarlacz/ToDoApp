8dd4bca1417f49daa752fd0b52268843221e72f6
Karol Garlacz <karol.garlacz@gmail.com>
2020-08-05 13:03:24 +0200

    Create a form for add task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
 class AddTask extends Component {
-  state = {};
+  state = {
+    text: "",
+    checked: false,
+  };
+
   render() {
     return (
-      <div>
-        <div>Dodaj tekst</div>
-        <hr />
+      <div className='form'>
+        <input
+          type='text'
+          placeholder='dodaj zadanie'
+          value={this.state.text}
+        />
+        <input type='checkbox' checked={this.state.checked} id='important' />
+        <label htmlFor='imporant'>Priorytet</label>
+        <label htmlFor='date'>Do kiedy zrobić</label>
+        <input
+          type='date'
+          value='2019-01-25'
+          min='2020-08/05'
+          max='2021-12-31'
+        />
+        <button>Dodaj</button>
       </div>
     );
   }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,37 @@
 import React from "react";
 
 const Task = (props) => {
-  const { text, date, id } = props.task;
-  return (
-    <div>
-      <p>
-        <strong>{text}</strong> - do <span>{date} </span>
-        <button onClick={() => props.change(id)}>Zostało zrobione</button>
-        <button onClick={() => props.delete(id)}>X</button>
-      </p>
-    </div>
-  );
+  const style = {
+    color: "red",
+  };
+
+  // destructuring
+  const { text, date, id, active, important, finishDate } = props.task;
+
+  if (active) {
+    return (
+      <div>
+        <p>
+          <strong style={important ? style : null}>{text}</strong> - do
+          <span> {date} </span>
+          <button onClick={() => props.change(id)}>Zostało zrobione</button>
+          <button onClick={() => props.delete(id)}>X</button>
+        </p>
+      </div>
+    );
+  } else {
+    const timeFinish = new Date(finishDate).toLocaleDateString();
+    return (
+      <div>
+        <p>
+          <strong>{text}</strong> <span>(zrobić do {date}) </span>
+          <br />
+          -potwierdzenie wykonania <span>{timeFinish} </span>
+          <button onClick={() => props.delete(id)}>X</button>
+        </p>
+      </div>
+    );
+  }
 };
 
 export default Task;
